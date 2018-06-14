4664ec20f4c361118686d35eb7ac8baabd66582f
frozmen <sisetskyi.a@gmail.com>
2018-06-14 18:20:54 +0300

    checkbox imp

--- app/src/main/java/com/appdevelopmentshop/validationPsample/ValidationFormController.java
+++ app/src/main/java/com/appdevelopmentshop/validationPsample/ValidationFormController.java
@@ -2,11 +2,14 @@ package com.appdevelopmentshop.validationPsample;
 
 import android.databinding.Bindable;
 import android.databinding.ObservableBoolean;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.appdevelopmentshop.validationPsample.databinding.LayoutValidationFormBinding;
 import com.appdevelopmentshop.validationp.Validator;
 import com.appdevelopmentshop.validationp.rules.BaseTextRule;
+import com.appdevelopmentshop.validationp.rules.CheckBoxRule;
 import com.appdevelopmentshop.validationp.rules.RegexRule;
 import com.appdevelopmentshop.validationp.rules.RequiredRule;
 import com.appdevelopmentshop.validationp.rules.Rule;
@@ -23,6 +26,7 @@ public class ValidationFormController extends Controller<LayoutValidationFormBin
     @Bindable public transient Validator validator = new Validator(true,this);
     public ObservableBoolean isBtnEnable = new ObservableBoolean(false);
 
+    //может переехать из контролеров в другое место
     public Rule[] rules = {new RequiredRule("Обязательное поле"),
             new RegexRule("[0-9]+", "Только цифры"), //пакетный рулл
             new BaseTextRule("Но цифры без 0 и 1") { //можно определить свой кастомный рулл
@@ -32,6 +36,14 @@ public class ValidationFormController extends Controller<LayoutValidationFormBin
                     return !(text.contains("0") || text.contains("1"));
                 }
             }};
+
+    //может переехать из контролеров в другое место
+    public Rule[] checkBoxRule = { new CheckBoxRule(true, new CheckBoxRule.ValidationErrorListener() {
+        @Override
+        public void onError(CheckBox checkBox) {
+            Toast.makeText(checkBox.getContext(), "Отметь чекбокс", Toast.LENGTH_LONG).show();
+        }
+    })};
 
     @Override
     public int getLayoutId() {
